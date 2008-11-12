825325dcbed18739e6ce315bb463e5f6275f9599
Carlo Zottmann (Rupert) <carlo@zottmann.org>
2008-11-12 20:36:33 +0100

    Initial import.

---
+++ twitteruserreputation.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name             Twitter User Reputation
+// @namespace        http://github.com/Carlo/greasekit-scripts/tree/master
+// @description      Twitter user reputation display, c/o TwerpScan.com
+// @include          http://twitter.com/*
+// @exclude          http://twitter.com/home
+// @author           Carlo Zottmann, <carlo@zottmann.org>
+// ==/UserScript==
+
+(function() {
+    if (!document.title.match(/^Twitter \/ \w+$/)) { 
+        return;
+    }
+
+    // Still unused
+    // var bgc  = document.defaultView.getComputedStyle(side, null).backgroundColor.replace(/[^\d\,]+/g, "");
+
+    var side = document.getElementById("side");
+    var u    = document.location.href.replace(/^.*twitter\.com\//,'');
+    var h    = ["<div class='section'>", 
+                "<div class='section-header'><h1>Reputation</h1></div>",
+                "<iframe src='http://twerpscan.com/embed/user_reputation.html?",
+                "user=", u, "' width='72' height='27' ",
+                "scrolling='no' marginheight='0' marginwidth='0' ",
+                "frameborder='0'></iframe>",
+                "</div>"].join("");
+
+    side.innerHTML = h + side.innerHTML;
+})();
