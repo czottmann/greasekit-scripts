b6e5aea40d4df7f9ec9d8f2267d8ddf470b0336c
Carlo Zottmann (Rupert) <carlo@zottmann.org>
2009-02-18 22:01:21 +0100

    Fixed links. Silly me.

--- fm4trackservice_lastfm.user.js
+++ fm4trackservice_lastfm.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Last.FM-Links im FM4 Trackservice
-// @version       1.0
+// @version       1.1
 // @namespace     http://github.com/Carlo/greasekit-scripts/tree/master 
 // @author        Carlo Zottmann <carlo@zottmann.org>
 // @include       http://fm4.orf.at/trackservicepopup/main
@@ -13,7 +13,7 @@
     var elem = elems.snapshotItem(0);
     
     var new_elem = elem.innerHTML;
-    new_elem = new_elem.replace( /(<b>([^<]*)<\/b>.*<i>([^<]*)<\/i>)/g, "$1 | <a href=\"http://last.fm/music/$2/_/$3\">Last.fm</a>" );
+    new_elem = new_elem.replace( /(<b>([^<]*)<\/b>.*<i>([^<]*)<\/i>)/g, "$1 | <a href=\"http://last.fm/music/$3/_/$2\">Last.fm</a>" );
     
     elem.innerHTML = new_elem;
   
