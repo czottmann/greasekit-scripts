df944eb72ad8fd401b3985254170aaf2fccc2815
Carlo Zottmann (Rupert) <carlo@zottmann.org>
2009-02-18 10:59:40 +0100

    Initial import.

---
+++ fm4trackservice_lastfm.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name          Last.FM-Links im FM4 Trackservice
+// @version       1.0
+// @namespace     http://github.com/Carlo/greasekit-scripts/tree/master 
+// @author        Carlo Zottmann <carlo@zottmann.org>
+// @include       http://fm4.orf.at/trackservicepopup/main
+// ==/UserScript==
+
+(function() {
+    var elems = document.evaluate('//table//table//td/font', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    if (!elems.snapshotLength) return;
+    
+    var elem = elems.snapshotItem(0);
+    
+    var new_elem = elem.innerHTML;
+    new_elem = new_elem.replace( /(<b>([^<]*)<\/b>.*<i>([^<]*)<\/i>)/g, "$1 | <a href=\"http://last.fm/music/$2/_/$3\">Last.fm</a>" );
+    
+    elem.innerHTML = new_elem;
+  
+})();
